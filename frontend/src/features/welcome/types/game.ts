4e67846cc5a9eea28928d5a529696349b92981f6
bobydevx <xthebobyx@gmail.com>
2026-07-12 18:54:49 +0200

    feat: agregado imagenes,paquetes y react router

---
+++ frontend/src/features/welcome/types/game.ts
@@ -0,0 +1,7 @@
+export type GameProps = {
+  id: number | string;
+  background_image: string;
+  name: string;
+  rating: string;
+  genres: [];
+};
---
+++ frontend/src/features/welcome/types/menu.ts
@@ -0,0 +1,7 @@
+export type MenuProps = {
+  id: number | string;
+  image: string;
+  name: string;
+  rating: string;
+  ingredients: [];
+};
