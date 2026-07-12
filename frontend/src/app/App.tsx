dfb24c9a99c415e81a62f314d152f64080a0b742
bobydevx <xthebobyx@gmail.com>
2026-07-12 18:54:49 +0200

    feat: agregado imagenes,paquetes y react router

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,7 +1,14 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
+import { Welcome } from "../features/welcome";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      {/* <Route index element={}></Route> */}
+      <Route path="welcome" element={<Welcome />} />
+    </Routes>
+  );
 }
 
 export default App;
