2d0279047830b43dab12afa7ca7ca32205d3ff49
bobydevx <xthebobyx@gmail.com>
2026-09-13 17:58:50 +0200

    feat: Agregado controlador y funciones iniciales

---
+++ backend/Cyberia.API/Controllers/CabinController.cs
@@ -0,0 +1,37 @@
+﻿using Cyberia.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cyberia.API.Controllers
+{
+    [Route("api/cabin")]
+    [ApiController]
+    public class CabinController : ControllerBase
+    {
+        private readonly CyberiaDbContext _context;
+        public CabinController(CyberiaDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetCabins()
+        {
+            var cabins = _context.Cabins.ToList();
+            return Ok(cabins);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCabinById([FromRoute] int id)
+        {
+            var cabin = _context.Cabins.Find(id);
+
+            if(cabin == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cabin);
+        }
+    }
+}
